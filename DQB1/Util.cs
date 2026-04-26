cee6c544e1bac1a7794b127249a726b2a0105a98
turtle-insect <turtle.insect@gmail.com>
2026-04-26 16:32:42 +0900

    refactor

--- DQB1/Util.cs
+++ DQB1/Util.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DQB1
+﻿namespace DQB1
 {
 	internal class Util
 	{
